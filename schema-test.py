09d54a2349d7d7dd393bc25ec704dbedc5c7b580
azaroth42 <azaroth42@gmail.com>
2021-02-15 17:02:15 -0500

    updates towards v1.0

--- schema-test.py
+++ schema-test.py
@@ -3,35 +3,48 @@ import json
 import jsonschema
 import os
 
-from jsonschema import validate, Draft6Validator
+from jsonschema import validate, Draft7Validator
 from jsonschema.exceptions import ValidationError
 import json
 
 
-fh = file('schema/schema-core.json')
-schema = json.load(fh)
-fh.close()
-v = Draft6Validator(schema)
+model_dirs = {
+	"place": "place",
+	"person": "person"
+}
 
-files = os.listdir('../linked.art/content/example/object/')
-for f in files:
-	if f.endswith('.json'):
-		fn = os.path.join('../linked.art/content/example/object/', f)
-		print "processing file: %s" % fn
-		fh = file(fn)
-		data = json.load(fh)
-		fh.close()
-		try:
-			v.validate(data)
-		except ValidationError as e:
-			print("Failed to validate %s" % fn)
-			if e.absolute_schema_path[-1] == u'required' and \
-				e.message.startswith("u'type'"):
-				continue
+base_instance_dir = "../linked.art/content/example"
+schema_dir = "schema"
 
-			print(e.message)
-			print(e.absolute_schema_path)
-			print(e.absolute_path)
-			break
-		print "validated!"
 
+
+for (k,v) in model_dirs.items():
+
+	schemafn = os.path.join(schema_dir, f"{v}.json")
+	fh = open(schemafn)
+	schema = json.load(fh)
+	fh.close()
+	v = Draft7Validator(schema)
+
+	exampledir = os.path.join(base_instance_dir, k)
+	files = os.listdir(exampledir)
+	for f in files:
+		if f.endswith('.json'):
+			fn = os.path.join(exampledir, f)
+			print("processing file: %s" % fn)
+			fh = open(fn)
+			data = json.load(fh)
+			fh.close()
+			try:
+				v.validate(data)
+			except ValidationError as e:
+				print("Failed to validate %s" % fn)
+				if e.absolute_schema_path[-1] == u'required' and \
+					e.message.startswith("u'type'"):
+					continue
+
+				print(e.message)
+				print(e.absolute_schema_path)
+				print(e.absolute_path)
+				break
+			print("validated!")
